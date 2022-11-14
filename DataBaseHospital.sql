8b2de7e685c08122033de2ec13671d241a57b82c
Fabiola Costa <110692074+FabiolaCosta@users.noreply.github.com>
2022-11-13 23:06:35 -0300

    Update DataBaseHospital.sql

--- DataBaseHospital.sql
+++ DataBaseHospital.sql
@@ -334,3 +334,18 @@ insert into plantao(id_plantao, data_plantao, hora_plantao, internacao_id, enfer
 insert into plantao(id_plantao, data_plantao, hora_plantao, internacao_id, enfermeiro_id) values (13, '2020-02-22', '09:00:00', 7, 3);
 insert into plantao(id_plantao, data_plantao, hora_plantao, internacao_id, enfermeiro_id) values (14, '2020-02-22', '20:30:00', 7, 1);
 
+
+-- Adicionando mais uma coluna 'em_atividade' na tabela medico
+--
+alter table medico add em_atividade varchar(100);
+
+update medico set em_atividade = 'Ativo' where id_medico = 1;
+update medico set em_atividade = 'Inativo' where id_medico = 2;
+update medico set em_atividade = 'Ativo' where id_medico = 3;
+update medico set em_atividade = 'Ativo' where id_medico = 4;
+update medico set em_atividade = 'Ativo' where id_medico = 5;
+update medico set em_atividade = 'Inativo' where id_medico = 6;
+update medico set em_atividade = 'Ativo' where id_medico = 7;
+update medico set em_atividade = 'Ativo' where id_medico = 8;
+update medico set em_atividade = 'Ativo' where id_medico = 9;
+update medico set em_atividade = 'Ativo' where id_medico = 10;
